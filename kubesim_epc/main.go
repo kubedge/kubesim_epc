a32c5aaf5a3359ce952a6a5702b4e0463b404e54
Robert Boyd <rb422w@att.com>
2018-10-13 20:42:37 +0000

    [XXXXX] import and call server
    Change-Id: I515ced4905c381a3a46f671b998ebf61a0eee824

--- kubesim_epc/main.go
+++ kubesim_epc/main.go
@@ -1,15 +1,20 @@
 package main
 import (
+  "log"
   "net/http"
   "strings"
+  "github.com/kubedge/kubesim_base/grpc/go/kubedge_server"
 )
 func sayHello(w http.ResponseWriter, r *http.Request) {
   message := r.URL.Path
   message = strings.TrimPrefix(message, "/")
-  message = "Hello " + message
+  message = "kubesim epc simulator " + message
   w.Write([]byte(message))
 }
 func main() {
+  log.Printf("%s", "kubesim_epc is running")
+  //run server forever
+  server.Server()
   http.HandleFunc("/", sayHello)
   if err := http.ListenAndServe(":8080", nil); err != nil {
     panic(err)
