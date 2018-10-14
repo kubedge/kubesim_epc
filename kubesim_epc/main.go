9c1a63975db8fc3ec600ffb39d083a3d8bda77a7
Robert Boyd <rb422w@att.com>
2018-10-14 04:11:31 +0000

    [XXXX] initial version of server using simbase using working config yaml library.
    Change-Id: I7a46b71f26ac9742ec6dff1ecaca77072edb72eb

--- kubesim_epc/main.go
+++ kubesim_epc/main.go
@@ -3,9 +3,10 @@ import (
   "log"
   "net/http"
   "strings"
-  "gopkg.in/yaml.v2"
-  "io/ioutil"
+  //"gopkg.in/yaml.v2"
+  //"io/ioutil"
   "github.com/kubedge/kubesim_base/grpc/go/kubedge_server"
+  "github.com/kubedge/kubesim_base/config"
 )
 func sayHello(w http.ResponseWriter, r *http.Request) {
   message := r.URL.Path
@@ -20,23 +21,9 @@ func main() {
   //this example is working, from http://sweetohm.net/article/go-yaml-parsers.en.html
   //previous example(not working):  https://stackoverflow.com/questions/30947534/reading-a-yaml-file-in-golang
 
-  type Config struct {
-      Foo string
-      Bar[] string
-  }
-
-  yamlFile, err := ioutil.ReadFile("config.yaml")
-  log.Printf("yamlFile=%s", string(yamlFile))
-  if err != nil {
-      log.Printf("yamlFile.Get err   #%v ", err)
-  }
-
-  var config Config
-  err = yaml.Unmarshal(yamlFile, &config)
-  if err != nil {
-      log.Fatalf("Unmarshal: %v", err)
-  }
-  log.Printf("Foo=%s, Bar=%s", config.Foo, config.Bar)
+  var conf config.Configdata
+  conf.Config()
+  log.Printf("Foo=%s, Bar=%s", conf.Foo, conf.Bar)
 
   //run server forever
   server.Server()
