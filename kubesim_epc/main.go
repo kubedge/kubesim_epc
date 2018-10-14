27a6f55bde4ca767d462de11cfb84d54013457e5
Robert Boyd <rb422w@att.com>
2018-10-14 00:50:12 +0000

    [XXXXX] initial checkin of yaml parser and config.yaml - not working anymore
    Change-Id: I79e489dba95b35f882ce234b11dc25a8b7710249

--- kubesim_epc/main.go
+++ kubesim_epc/main.go
@@ -3,6 +3,8 @@ import (
   "log"
   "net/http"
   "strings"
+  "gopkg.in/yaml.v2"
+  "io/ioutil"
   "github.com/kubedge/kubesim_base/grpc/go/kubedge_server"
 )
 func sayHello(w http.ResponseWriter, r *http.Request) {
@@ -13,6 +15,31 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 }
 func main() {
   log.Printf("%s", "kubesim_epc is running")
+
+
+  //test reading yaml
+  //This (or close) was working earlier but not after I ran:  sudo go run main.go
+  //from example:  https://stackoverflow.com/questions/30947534/reading-a-yaml-file-in-golang
+
+  type Config struct {
+      Hits int64 `yaml:"hits"`
+      Time int64 `yaml:"time"`
+  }
+
+  yamlFile, err := ioutil.ReadFile("config.yaml")
+  log.Printf("yamlFile=%s", string(yamlFile))
+  if err != nil {
+      log.Printf("yamlFile.Get err   #%v ", err)
+  }
+
+  var config Config
+  //config.Hits = 9999  //DEBUG only
+  err = yaml.Unmarshal(yamlFile, &config)
+  if err != nil {
+      log.Fatalf("Unmarshal: %v", err)
+  }
+  log.Printf("hits=%d, time=%d", config.Hits, config.Time)
+
   //run server forever
   server.Server()
   http.HandleFunc("/", sayHello)
