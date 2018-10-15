4f526c655b718fe76b3488e98baf8a6aeaecbc31
Robert Boyd <rb422w@att.com>
2018-10-15 21:56:55 +0000

    [XXXXX] add call to connected library to test methods    NOTE: will be moving to clients.
    Change-Id: Idfba106f36ef147ca951ef244ce648c3f667ccdb

--- kubesim_epc/main.go
+++ kubesim_epc/main.go
@@ -7,6 +7,7 @@ import (
   //"io/ioutil"
   "github.com/kubedge/kubesim_base/grpc/go/kubedge_server"
   "github.com/kubedge/kubesim_base/config"
+  "github.com/kubedge/kubesim_base/connected"
 )
 func sayHello(w http.ResponseWriter, r *http.Request) {
   message := r.URL.Path
@@ -25,6 +26,10 @@ func main() {
   conf.Config()
   log.Printf("epc server:  product_name=%s, product_type=%s, product_family=%s, product_release=%s, feature_set1=%s, feature_set2=%s",
              conf.Product_name, conf.Product_type, conf.Product_family, conf.Product_release, conf.Feature_set1, conf.Feature_set2)
+
+  var conn connected.Connecteddata
+  conn.Readconnectvalues()
+  log.Printf("epc server:  connected=%s", conn.Connected)
 
   //run server forever
   server.Server()
